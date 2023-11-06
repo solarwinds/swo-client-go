e1fb9862fa48d7c41eab97f9ce29d010a2fe723e
root <root@DESKTOP-TJP1I4Q>
2023-11-06 11:26:59 -0600

    unit tests

--- pkg/client/website_test.go
+++ pkg/client/website_test.go
@@ -10,16 +10,6 @@ import (
 	"github.com/google/uuid"
 )
 
-var (
-	websiteMockData = struct {
-		fieldName string
-		fieldUrl  string
-	}{
-		"swo-client-go - website",
-		"www.solarwinds.com",
-	}
-)
-
 func GetCreateInput() CreateWebsiteInput {
 	inputJson, err := ioutil.ReadFile("website_test.json")
 	if err != nil {
@@ -51,26 +41,6 @@ func TestSwoService_ReadWebsite(t *testing.T) {
 			t.Errorf("Request got = %+v, want = %+v", got, want)
 		}
 
-		// response := &getWebsiteByIdEntitiesEntityQueriesByIdWebsite{
-		// 	Name: &website.Name,
-		// 	Url:  website.Url,
-		// 	Monitoring: &getWebsiteByIdEntitiesEntityQueriesByIdWebsiteMonitoring{
-		// 		Availability: &getWebsiteByIdEntitiesEntityQueriesByIdWebsiteMonitoringAvailabilityAvailabilityMonitoring{
-		// 			Protocols: website.AvailabilityCheckSettings.Protocols,
-		// 		},
-		// 		CustomHeaders: []getWebsiteByIdEntitiesEntityQueriesByIdWebsiteMonitoringCustomHeadersCustomHeader{
-		// 			{
-		// 				Name:  website.AvailabilityCheckSettings.CustomHeaders[0].Name,
-		// 				Value: website.AvailabilityCheckSettings.CustomHeaders[0].Value,
-		// 			},
-		// 		},
-		// 		Rum: &getWebsiteByIdEntitiesEntityQueriesByIdWebsiteMonitoringRumRumMonitoring{
-		// 			ApdexTimeInSeconds: &website.AvailabilityCheckSettings.TestIntervalInSeconds,
-		// 			Spa:                *website.Rum.Spa,
-		// 		},
-		// 	},
-		// }
-
 		sendGraphQLResponse(t, w, getWebsiteByIdResponse{
 			Entities: getWebsiteByIdEntitiesEntityQueries{},
 		})
@@ -86,7 +56,7 @@ func TestSwoService_ReadWebsite(t *testing.T) {
 	}
 
 	if !testObjects(t, got, want) {
-		t.Errorf("Swo.ReadNotification returned %+v, wanted %+v", got, want)
+		t.Errorf("Swo.ReadWebsite returned %+v, wanted %+v", got, want)
 	}
 }
 
@@ -129,7 +99,7 @@ func TestSwoService_CreateWebsite(t *testing.T) {
 	}
 
 	if !testObjects(t, got, want) {
-		t.Errorf("Swo.CreateNotification returned %+v, want %+v", got, want)
+		t.Errorf("Swo.CreateWebsite returned %+v, want %+v", got, want)
 	}
 }
 
@@ -138,7 +108,9 @@ func TestSwoService_UpdateWebsite(t *testing.T) {
 	defer teardown()
 
 	input := UpdateWebsiteInput{
-		Id: "123",
+		Id:   "123",
+		Name: "swo-client-go - website",
+		Url:  "www.solarwinds.com",
 	}
 
 	server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -172,9 +144,7 @@ func TestSwoService_DeleteWebsite(t *testing.T) {
 	ctx, client, server, _, teardown := setup()
 	defer teardown()
 
-	input := DeleteWebsiteInput{
-		Id: "123",
-	}
+	input := "123"
 
 	server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		gqlInput, err := getGraphQLInput[__deleteWebsiteMutationInput](r)
@@ -182,7 +152,7 @@ func TestSwoService_DeleteWebsite(t *testing.T) {
 			t.Errorf("Swo.DeleteWebsite error: %v", err)
 		}
 
-		got := gqlInput.Input
+		got := gqlInput.Input.Id
 		want := input
 
 		if !testObjects(t, got, want) {
@@ -192,13 +162,13 @@ func TestSwoService_DeleteWebsite(t *testing.T) {
 		sendGraphQLResponse(t, w, deleteWebsiteMutationResponse{
 			Dem: deleteWebsiteMutationDemDemMutations{
 				DeleteWebsite: deleteWebsiteMutationDemDemMutationsDeleteWebsiteDeleteWebsiteSuccess{
-					Id: input.Id,
+					Id: input,
 				},
 			},
 		})
 	})
 
-	err := client.WebsiteService().Delete(ctx, input.Id)
+	err := client.WebsiteService().Delete(ctx, input)
 	if err != nil {
 		t.Errorf("Swo.DeleteWebsite returned error: %v", err)
 	}
@@ -227,41 +197,3 @@ func TestSwoService_WebsiteServerErrors(t *testing.T) {
 		t.Error("Swo.WebsiteServerErrors expected an error response")
 	}
 }
-
-// func TestWebsite_Marshal(t *testing.T) {
-// 	testJSONMarshal(t, &ReadWebsiteResult{}, "{}")
-
-// 	id := uuid.NewString()
-// 	got := ReadWebsiteResult{
-// 		Entities: getWebsiteByIdEntitiesEntityQueries{
-// 			ById: &getWebsiteByIdEntitiesEntityQueriesByIdEntity{},
-// 		},
-// 	}
-
-// 	want := fmt.Sprintf(`
-// 	{
-// 		"id": "%s",
-// 		"type": "email",
-// 		"title": "%s",
-// 		"description": "%s",
-// 		"settings": {
-// 			"addresses": [
-// 				{
-// 					"email": "test1@host.com"
-// 				},
-// 				{
-// 					"email": "test2@host.com"
-// 				}
-// 			]
-// 		},
-// 		"createdAt": "%s",
-// 		"createdBy": "%s"
-// 	}`,
-// 		id,
-// 		notificationsMockData.fieldTitle,
-// 		notificationsMockData.fieldDesc,
-// 		notificationsMockData.fieldCreatedAt.Format(time.RFC3339),
-// 		notificationsMockData.fieldCreatedBy)
-
-// 	testJSONMarshal(t, got, want)
-// }
